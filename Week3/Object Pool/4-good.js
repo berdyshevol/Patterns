bd4aeb07a43756e5a80ede08249c59fea19e8d73
Oleg Berdyshev <berdyshevo@gmail.com>
2025-06-09 16:33:22 -0500

    Refactor poolify function to improve separation of concerns and optimize instance management

--- Week3/Object Pool/4-good.js
+++ Week3/Object Pool/4-good.js
@@ -1,28 +1,21 @@
 "use strict";
 
-// TODO: Refactor to respect SoC principle and
-// optimize JavaScript for V8
-
 const poolify = (factory, options, size, max) => {
-  const instances = []; // Preallocate array
-  for (let i = 0; i < size; i++) {
-    // Use Array methods instead of loop
-    const instance = factory(...options); // Avoid array destructuring
-    instances.push(instance);
-  }
+  const createInstance = (options) => factory(...options);
 
-  return (instance) => {
-    // Respect SoC and SOLID/SRP
-    if (instance) {
-      // Avoid if-statement
-      if (instances.length < max) {
-        instances.push(instance);
-      }
+  const instances = new Array(size)
+    .fill(null)
+    .map(() => createInstance(options));
+
+  const acquire = () => instances.pop() || createInstance(options);
+
+  const release = (instance) => {
+    if (instances.length < max) {
+      instances.push(instance);
     }
-    instance = instances.pop(); // Do not reassign incoming parameters
-    if (!instance) instance = factory(...options);
-    return instance;
   };
+
+  return { acquire, release };
 };
 
 // Usage
@@ -30,6 +23,6 @@ const poolify = (factory, options, size, max) => {
 const createBuffer = (size) => new Uint8Array(size);
 const pool = poolify(createBuffer, [4096], 10, 15);
 
-const instance = pool();
+const instance = pool.acquire();
 console.log({ instance });
-pool(instance);
+pool.release(instance);
