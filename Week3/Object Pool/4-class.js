d15ebe26809b8baa6d4644e4a86c6de6af758249
Oleg Berdyshev <berdyshevo@gmail.com>
2025-06-09 17:23:53 -0500

    Implement Object Pool class in 4-class.js

---
+++ Week3/Object Pool/4-class.js
@@ -0,0 +1,37 @@
+"use strict";
+
+class Pool {
+  #factory;
+  #size;
+  #max;
+  #instances;
+
+  constructor(factory, { size, max }) {
+    this.#factory = factory;
+    this.#size = size;
+    this.#max = max;
+    this.#instances = new Array(this.#size).fill(null).map(() => factory());
+  }
+
+  acquire() {
+    return this.#instances.pop() || this.#factory();
+  }
+
+  release(instance) {
+    if (this.#instances.length < this.#max) {
+      this.#instances.push(instance);
+    }
+  }
+}
+
+// Usage
+
+const createBuffer = (size) => new Uint8Array(size);
+
+const FILE_BUFFER_SIZE = 4096;
+const createFileBuffer = () => createBuffer(FILE_BUFFER_SIZE);
+
+const pool = new Pool(createFileBuffer, { size: 10, max: 15 });
+const instance = pool.acquire();
+console.log({ instance });
+pool.release(instance);
